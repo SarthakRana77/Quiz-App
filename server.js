346195b712e1cecd5fbc8e470f423f00c7cfbf5d
Sarthak Rana <105589002+SarthakRana77@users.noreply.github.com>
2025-05-03 13:26:36 +0530

    Update server.js

--- server.js
+++ server.js
@@ -80,6 +80,6 @@ app.get('/api/high-scores', (req, res) => {
     res.json(highestScores);
 });
 
-app.listen(PORT, () => {
-    console.log(`Server running on http://localhost:${PORT}`);
+app.listen(PORT, '0.0.0.0', () => {
+  console.log(`Server running on http://localhost:${PORT}`);
 });
